532a667961d37fb1e7f1a0016d38d875b5a14422
BigStar2024 <aadsf7463@gmail.com>
2024-06-18 16:54:05 +0900

    update

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'https://park-charge-management-back.onrender.com';
-export const BASE_URL = 'https://01ed-83-234-227-51.ngrok-free.app';
+export const BASE_URL = 'https://vital-stingray-tender.ngrok-free.app';
