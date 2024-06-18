12608c25ab62bc3c1e65ba3025ff53e724045be6
BigStar2024 <aadsf7463@gmail.com>
2024-06-18 16:37:18 +0900

    date

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://park-charge-management-back.onrender.com';
-export const BASE_URL = 'http://88.99.90.19:4242';
+export const BASE_URL = 'https://park-charge-management-back.onrender.com';
+// export const BASE_URL = 'http://88.99.90.19:4242';
