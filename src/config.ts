f33034e325ebf42d231169a1a7eb09ab01506721
BigStar2024 <aadsf7463@gmail.com>
2024-06-27 22:49:55 +0900

    update the base ngrok url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'https://park-charge-management-back.onrender.com';
-export const BASE_URL = 'https://df50-88-99-90-19.ngrok-free.app';
+export const BASE_URL = 'https://bfba-88-99-90-19.ngrok-free.app';
